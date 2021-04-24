3adbb3bc180d93436aaa23b33758350d6fef0545
ckymn <cokyamanmuhammet@gmail.com>
2021-04-24 11:04:49 +0300

    .

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,25 +1,51 @@
+// 1.constructor --> state initialization
+// 2.render work
+// 3.onClick eventi çalıştı 
+// 4.showMore worked
+// 5.setState state nesnesini guncelledi --> 6.triggered render
+// 7.render metodu guncel JSX i sayfada gosteriyor
+
 import React from "react";
 
-const Collapse = (props) => {
-  console.log(props);
-  return (
-    <div>
-        <a
-        className="btn btn-warning"
-          data-bs-toggle="collapse"
-          href="#collapseExample"
-          role="button"
-          aria-expanded="false"
-          aria-controls="collapseExample"
-          style={{ display: "block" }}
-        >
-          OPEN CARD
-        </a>
-        <div className="collapse" id="collapseExample">
-          {props.children}
-        </div>
-    </div>
-  );
-};
+class Collapse extends React.Component {
+  constructor() {
+    console.log("constructor olusturuldu...");
+    super();
+    this.state = {
+      showContent: false,
+    };
+  }
+  showMore = () => {
+    this.setState({ showContent: !this.state.showContent });
+  };
+
+  componentDidMount() {
+    console.log("Component Olusturuldu");
+  }
+
+  componentDidUpdate() {
+    console.log("Component Guncellendi");
+  }
+
+  render() {
+    console.log("render calisti...");
+    return (
+      <div>
+        <button className="btn btn-primary w-100" onClick={this.showMore}>
+          {React.Children.map(
+            this.props.children,
+            (children) => children.props.cardTitle
+          )}
+        </button>
+
+        {this.state.showContent ? (
+          <div className="collapse show">
+            {React.Children.map(this.props.children, (children) => children)}
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+}
 
 export default Collapse;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,64 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom'
+
+import Card from './components/Card';
+import Collapse from './components/Collapse';
+
+
+const App = () => {
+
+
+  return (
+    <div className="container">
+
+      <div className="row">
+
+        <div className="card-group w-100">
+
+          <div className="col">
+
+            <Collapse href="collapseExample1">
+              <Card
+                cardText="Lorem Ipsum Text I"
+                updatedTime="Last Updated 1 min ago"
+                image="https://picsum.photos/id/1/200/300"
+              />
+            </Collapse>
+
+          </div>
+
+          <div className="col">
+
+            <Collapse href="collapseExample2">
+              <Card
+                cardTitle="TEST TITLEX"
+                cardText="Lorem Ipsum Text II"
+                updatedTime="Last Updated 2 min ago"
+                image="https://picsum.photos/id/2/200/300"
+              />
+            </Collapse>
+
+          </div>
+
+          <div className="col">
+
+            <Collapse href="collapseExample3">
+              <Card
+
+                cardText="Lorem Ipsum Text III"
+                updatedTime="Last Updated 3 min ago"
+                image="https://picsum.photos/id/3/200/300"
+              />
+            </Collapse>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App name="muhammet"/>
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
