16b53e3301d60e0a0d4c8af5cbcfa6d29770bb9c
ckymn <cokyamanmuhammet@gmail.com>
2021-04-29 02:27:52 +0300

    code fix

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,83 @@
 import React, { Component } from "react";
 import SearchBar from "../SearchBar";
 import MovieList from "../MovieList";
+import CreateMovie from "../CreateMovie";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import axios from "axios";
+// require("dotenv").config();
 
 export default class index extends Component {
+  constructor(props) {
+    super(props);
+    this.deleteMovie = this.deleteMovie.bind(this);
+
+    this.state = {
+      movies: [],
+      searchQuery: "",
+    };
+  }
+
+  async componentDidMount() {
+    // const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API}&language=en-US&page=1`);
+    const response = await axios.get(`http://localhost:3001/movies`);
+    this.setState({
+      movies: response.data,
+    });
+  }
+
+  deleteMovie = (movie) => {
+    axios.delete(`http://localhost:3001/movies/${movie.id}`);
+    const newMovie = this.state.movies.filter((i) => i.id !== movie.id);
+
+    this.setState({
+      movies: newMovie,
+    });
+  };
+
+  searchMovie = (e) => {
+    this.setState({
+      searchQuery: e.target.value,
+    });
+  };
+
   render() {
+    let search = this.state.movies.filter(
+      (i) =>
+        i.name
+          .toLocaleLowerCase()
+          .indexOf(this.state.searchQuery.toLocaleLowerCase()) !== -1
+    );
     return (
-      <div className="container" style={{ textAlign: "center" }}>
-        <div className="row">
+      <Router>
+        <div className="container" style={{ textAlign: "center" }}>
+          <div className="row">
             <div className="d-flex flex-column">
-                <h1>Movies Porjects</h1>
-                <SearchBar />
-                <MovieList />
+              <Switch>
+
+                <Route
+                  path="/"
+                  exact
+                  render={() => (
+                    <React.Fragment>
+                      <h1 style={{ fontFamily: "'Abril Fatface', cursive" }}>
+                        FUJI.3.776
+                      </h1>
+                      <SearchBar searchMovieProps={this.searchMovie} />
+                      <MovieList
+                        movie={search}
+                        deleteMovieProps={this.deleteMovie}
+                      />
+                    </React.Fragment>
+                  )}
+                ></Route>
+
+                <Route path="/add" component={CreateMovie}></Route>
+
+              </Switch>
             </div>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,16 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class index extends Component {
-    render() {
-        return (
-            <div className="row">
-                <div className="col-sm-10">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                </div>
-                <div className="col-sm-2">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </div>
-            </div>
-        )
-    }
+
+  state={
+    open : false,
+  }
+
+  searchHeandle = (e) => {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="row" style={{ margin: "20px" }}>
+        <div className="col-sm-10">
+          <input
+            onSubmit={this.searchHeandle}
+            onChange={this.props.searchMovieProps}
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          ></input>
+        </div>
+        <div className="col-sm-2">
+          <button className="btn btn-success" type="submit">
+            Create 
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
